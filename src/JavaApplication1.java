b2ab114a532b8c790be1e798438454c1cc478769
amal_almutairi <mollly18@gmail.com>
2023-09-05 01:00:58 +0300

    first

---
+++ src/JavaApplication1.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here 
+        System.out.println("hello my name is Amal almutairi"); 
+  
+    }
+    
+}
